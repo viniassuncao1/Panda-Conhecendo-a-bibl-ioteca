f47335c9943dc1cfcf806da56623de0a575917c5
viniassuncao1 <viniciusmassuncao@gmail.com>
2025-04-10 18:21:16 -0300

    Testando Pandas

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-import pandas as pd
-print("Pandas version:", pd.__version__)
+import pandas as pd # Importa a biblioteca pandas, que é amplamente utilizada para manipulação e análise de dados em Python.
+
+url = 'https://raw.githubusercontent.com/alura-cursos/pandas-conhecendo-a-biblioteca/main/desafios/alunos.csv'
+dados = pd.read_csv(url)
+#print(dados)
+
+print(dados.head(7))
+print(dados.tail(5))
+print(dados.shape)
+print(type(dados))
+print(dados.columns)
+
+print(dados[['Nome', 'Idade']])                                                                                                         
+print(dados.describe())
